986bbb722c4eba5e166b0c9af3c804adb26c3f90
BobbySU <radio_20bg@yahoo.com>
2022-04-22 19:10:52 +0300

    Update

--- Syntax, Functions and Statements/03Largest Number.js
+++ Syntax, Functions and Statements/03Largest Number.js
@@ -1,5 +1,4 @@
 function solve(num1, num2, num3) {
-    let result = Math.max(num1,num2,num3);
     console.log(`The largest number is ${Math.max(num1,num2,num3)}.`);
 }
 
---
+++ Syntax, Functions and Statements/04CircleArea.js
@@ -0,0 +1,6 @@
+function solve(input) {
+    console.log(input)
+}
+
+solve(5)
+solve('name')
