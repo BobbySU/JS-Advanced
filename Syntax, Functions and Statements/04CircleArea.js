ea40a1848c5d54ecae21c8df50cfdb1408d02c3b
BobbySU <radio_20bg@yahoo.com>
2022-04-23 11:50:02 +0300

    Add 04. Circle Area

--- Syntax, Functions and Statements/04CircleArea.js
+++ Syntax, Functions and Statements/04CircleArea.js
@@ -1,5 +1,9 @@
 function solve(input) {
-    console.log(input)
+    if(typeof input === "number"){
+        console.log((Math.pow(input, 2) * Math.PI).toFixed(2))
+    }else{
+        console.log(`We can not calculate the circle area, because we receive a ${typeof input}.`)
+    }
 }
 
 solve(5)
