a4713bba0666b6e925117d2689d1bbd54f7cce9f
BobbySU <radio_20bg@yahoo.com>
2022-04-25 03:47:05 +0300

    Add Road Radar

---
+++ Syntax, Functions and Statements - Exercise/06RoadRadar.js
@@ -0,0 +1,37 @@
+function solve(speed, area) {
+    let limit = 0;
+    let status = '';
+    let difference = 0;
+    switch (area) {
+        case 'motorway':
+            limit = 130;
+            break;
+        case 'interstate':
+            limit = 90;
+            break;
+        case 'city':
+            limit = 50;
+            break;
+        case 'residential':
+            limit = 20;
+            break;
+    }
+    if (speed > limit) {
+        difference = speed - limit;
+        if (difference <= 20) {
+            status = 'speeding';
+        } else if (difference <= 40) {
+            status = 'excessive speeding';
+        } else {
+            status = 'reckless driving';
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${status}`);
+    } else {
+        console.log(`Driving ${speed} km/h in a ${limit} zone`);
+    }
+}
+
+solve(40, 'city')
+solve(21, 'residential')
+solve(120, 'interstate')
+solve(200, 'motorway')
