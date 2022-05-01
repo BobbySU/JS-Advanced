29b6e98b34a010844ae3555d05f216ce92c6f43d
BobbySU <radio_20bg@yahoo.com>
2022-05-01 20:51:26 +0300

    Add Even Position Elements

---
+++ Arrays and Nested Arrays/01EvenPositionElements.js
@@ -0,0 +1,12 @@
+function solve(input) {
+    let result = '';
+    for (let index = 0; index < input.length; index+=2) {
+        result += input[index];
+        result += ' ';
+    } 
+    console.log(result)
+}
+
+solve(['20', '30', '40', '50', '60'])
+solve(['5', '10'])
+solve(['5', '10', '2'])
