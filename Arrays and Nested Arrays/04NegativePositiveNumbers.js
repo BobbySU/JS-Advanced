1eb2e348b22e7f604f84c0c4b02ca9bc5ddd0e63
BobbySU <radio_20bg@yahoo.com>
2022-05-03 21:42:37 +0300

    Add Negative / Positive Numbers

---
+++ Arrays and Nested Arrays/04NegativePositiveNumbers.js
@@ -0,0 +1,14 @@
+function solve(input) {
+    const result = [];
+    for (const num of input) {
+        if(num < 0){
+                result.unshift(num)
+        }else{
+                result.push(num)
+        }
+    }
+    return result.join('\n');
+}
+
+console.log(solve([7, -2, 8, 9]))
+console.log(solve([3, -2, 0, -1]))
