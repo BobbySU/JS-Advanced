0b7dd5592aac9f8938cfafc0aef45fa291a1221f
BobbySU <radio_20bg@yahoo.com>
2022-05-04 00:59:09 +0300

    Update Process Odd Positions

--- Arrays and Nested Arrays/08ProcessOddPositions.js
+++ Arrays and Nested Arrays/08ProcessOddPositions.js
@@ -1,10 +1,18 @@
 function solve(input) {
+    return input
+        .filter((v, i) => i % 2 == 1)
+        .map(x => x * 2)
+        .reverse()
+        .join(' ')
+}
+
+function solve1(input) {
     let result = []
     for (let i = 1; i < input.length; i += 2) {
         result.unshift(input[i] * 2)
     }
-    return result;
+    return result.join(' ');
 }
 
 console.log(solve([10, 15, 20, 25]))
-console.log(solve([3, 0, 10, 4, 7, 3]))
+console.log(solve1([3, 0, 10, 4, 7, 3]))
