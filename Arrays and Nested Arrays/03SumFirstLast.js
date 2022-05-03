880bcc4fc74a8c817d71f1c7cbc00399783904e3
BobbySU <radio_20bg@yahoo.com>
2022-05-03 21:08:41 +0300

    Add Sum First Last

---
+++ Arrays and Nested Arrays/03SumFirstLast.js
@@ -0,0 +1,10 @@
+function solve(input) {
+    const first = Number([...input].shift())
+    const last = Number([...input].pop())
+    return first + last;
+}
+
+console.log(solve(['20', '30', '40']))
+console.log(solve(['5', '10']))
+console.log(solve(['10']))
+
