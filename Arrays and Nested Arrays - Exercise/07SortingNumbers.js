6f9196cbb5b06ca563fb621aa51f1ff5b0cc2ebc
BobbySU <radio_20bg@yahoo.com>
2022-05-13 00:06:08 +0300

    Add Sorting Numbers Update

--- Arrays and Nested Arrays - Exercise/07SortingNumbers.js
+++ Arrays and Nested Arrays - Exercise/07SortingNumbers.js
@@ -8,5 +8,18 @@ function solve(matrix) {
     return result;
 }
 
+function solve2(matrix) {
+    matrix.sort((a, b) => a - b);
+    const result = [];
+    const mid = Math.floor(matrix.length / 2);
+    for (let i = 0; i < mid; i++) {
+        result.push(matrix[i])
+        result.push(matrix[matrix.length - 1 - i])
+    }
+    return result;
+}
+
 console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]
+))
+console.log(solve2([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]
 ))
