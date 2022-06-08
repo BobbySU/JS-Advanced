52a5365221159f410227c40057f7f605528ece0c
BobbySU <radio_20bg@yahoo.com>
2022-06-08 20:56:05 +0300

    Update Rectangle

--- Objects and Composition - Exercise/08Rectangle.js
+++ Objects and Composition - Exercise/08Rectangle.js
@@ -1,17 +1,16 @@
 function rectangle(width, height, color) {
-    class Rectangle{
-        constructor(width, height, color){
+    class Rectangle {
+        constructor(width, height, color) {
             this.width = Number(width);
             this.height = Number(height);
             this.color = color[0].toUpperCase() + color.slice(1);
-           
         }
-    calcArea(){return this.width*this.height;
+        calcArea() {
+            return this.width * this.height;
+        }
     }
-}   
     return new Rectangle(width, height, color);
 }
-
 
 let rect = rectangle(4, 5, 'red');
 console.log(rect.width);
