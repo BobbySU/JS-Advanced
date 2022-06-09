849732504ff037e7bcfb5e260d497ef4d914f7c3
BobbySU <radio_20bg@yahoo.com>
2022-06-10 00:07:39 +0300

    Add Pascal or Camel Case

--- DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO...
+    let text = document.getElementById('text').value.toLowerCase();
+    let type = document.getElementById('naming-convention').value;
+    let textArr = text.split(' ');
+    let result = '';
+    if (type === 'Camel Case') {
+        result += textArr[0];
+        for (let i = 1; i < textArr.length; i++) {
+            result += textArr[i][0].toUpperCase() + textArr[i].slice(1, textArr[i].length);
+        }
+    } else if (type === 'Pascal Case') {
+        for (let i = 0; i < textArr.length; i++) {
+            result += textArr[i][0].toUpperCase() + textArr[i].slice(1, textArr[i].length);
+        }
+    } else {
+        result = "Error!";
+    }
+    document.getElementById('result').textContent = result;
 }
