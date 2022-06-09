5e2cf6a699bb2e9ad34a7dc9d1e11bf29ef6bd1d
BobbySU <radio_20bg@yahoo.com>
2022-06-09 22:35:31 +0300

    Add Extract Parenthesis

--- DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
-
+    const text = document.getElementById('content').textContent;
+    let result = [];
+    const pattern = /\((.+?)\)/g;
+    let match = pattern.exec(text);
+    while (match) {
+        result.push(match[1]);
+        match = pattern.exec(text);
+    }
+    //console.log(result.join('; '));
+    return result.join('; ');
 }
