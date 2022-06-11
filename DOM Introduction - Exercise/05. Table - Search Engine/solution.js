6a988c7426d1f2abf70ed99f86a78e3a79590a49
BobbySU <radio_20bg@yahoo.com>
2022-06-12 01:09:00 +0300

    Add Table – Search Engine

--- DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
-
-   }
+    function onClick() {
+        let list = Array.from(document.querySelectorAll("tbody tr"));
+        let search = document.getElementById('searchField');
+        for (const element of list) {
+            element.classList.remove("select");
+            if (search.value != '') {
+                if (element.innerHTML.includes(search.value)) {
+                    element.classList.add("select");
+                }
+            }
+        }
+        search.value = '';
+    }
 }
