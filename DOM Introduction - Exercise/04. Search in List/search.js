ad82b1d6dfbdf3b7cacaca0b469685f6cbf49008
BobbySU <radio_20bg@yahoo.com>
2022-06-12 00:12:04 +0300

    Add Search in List

--- DOM Introduction - Exercise/04. Search in List/search.js
+++ DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,16 @@
 function search() {
-   // TODO
+    let list = Array.from(document.querySelectorAll("#towns li"));
+    let search = document.getElementById('searchText').value;
+    let matches = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].textContent.includes(search)) {
+            matches++;
+            list[i].style.fontWeight = "bold";
+            list[i].style.textDecoration = "underline";
+        } else {
+            list[i].style.fontWeight = "normal";
+            list[i].style.textDecoration = "none";
+        }
+    }
+    document.getElementById('result').textContent = `${matches} matches found`;
 }
