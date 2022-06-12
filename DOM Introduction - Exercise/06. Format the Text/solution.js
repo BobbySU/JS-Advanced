b9feccb427f41ede02d0ddafb633dd832dccd1b9
BobbySU <radio_20bg@yahoo.com>
2022-06-12 13:19:48 +0300

    Add Format the Text

--- DOM Introduction - Exercise/06. Format the Text/solution.js
+++ DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,10 @@
 function solve() {
-  //TODO
+    let text = document.getElementById('input').value;
+    let sentences = text.split('.')
+        .filter(e => e != '')
+        .map(e => e + '.');
+    let element = Math.ceil(sentences.length / 3);
+    for (let i = 0; i < element; i++) {
+        document.getElementById('output').innerHTML += `<p>${sentences.splice(0, 3).join('')}</p>`;
+    }
 }
