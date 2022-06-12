544e59886062a226f4f21a1c626dc5b098ae02d0
BobbySU <radio_20bg@yahoo.com>
2022-06-13 00:29:34 +0300

    Add Delete from Table

--- DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,13 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const email = document.querySelector('input[name="email"]').value;
+    const list = Array.from(document.querySelectorAll('tbody tr'))
+    for (const row of list) {
+        if (row.children[1].textContent == email) {
+            const parent = row.parentElement;
+            parent.removeChild(row);
+            document.getElementById('result').textContent = 'Deleted.';
+        } else {
+            document.getElementById('result').textContent = 'Not found.';
+        }
+    }
 }
