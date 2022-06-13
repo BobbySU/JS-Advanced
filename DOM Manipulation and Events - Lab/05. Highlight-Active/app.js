d18ec15974a1e4fb75a5d9fc6bcc3072e7a3de5e
BobbySU <radio_20bg@yahoo.com>
2022-06-14 01:38:02 +0300

    Add Highlight Active

--- DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    Array.from(document.querySelectorAll('input'))
+        .forEach(e => {
+                e.addEventListener('focus', onFocus);
+                e.addEventListener('blur', onBlur);
+            }
+        );
+
+    function onFocus(event) {
+        event.target.parentElement.className = 'focused';
+    }
+
+    function onBlur(event) {
+        event.target.parentElement.className = '';
+    }
 }
