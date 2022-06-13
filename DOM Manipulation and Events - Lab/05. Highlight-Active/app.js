eb06140fa884884eb1741c1ae73bdbc6eefb839f
BobbySU <radio_20bg@yahoo.com>
2022-06-14 01:45:07 +0300

    Update Highlight Active

--- DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -8,9 +8,11 @@ function focused() {
 
     function onFocus(event) {
         event.target.parentElement.className = 'focused';
+        // event.target.parentElement.classList.add('focused');
     }
 
     function onBlur(event) {
         event.target.parentElement.className = '';
+        // event.target.parentElement.classList.remove('focused');
     }
 }
