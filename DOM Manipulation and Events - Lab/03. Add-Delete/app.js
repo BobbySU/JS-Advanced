45f25c60b8abd0b6ea29b661c20d360635e6ef09
BobbySU <radio_20bg@yahoo.com>
2022-06-14 00:32:51 +0300

    Add Add / Delete

--- DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    const input = document.getElementById('newItemText');
+    if (input.value != '') {
+        let liElenment = document.createElement('li');
+        liElenment.textContent = input.value;
+        const deleteBtn = document.createElement('a');
+        deleteBtn.textContent = '[Delete]';
+        deleteBtn.href = '#';
+        liElenment.appendChild(deleteBtn);
+        deleteBtn.addEventListener('click', onDelete)
+        document.getElementById('items').appendChild(liElenment);
+        input.value = '';
+    }
+
+    function onDelete(event) {
+        event.target.parentElement.remove()
+    }
 }
+
