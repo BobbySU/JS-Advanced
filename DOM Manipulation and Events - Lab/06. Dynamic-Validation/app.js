ecc2a0fe9db3b774fd12f1b92b246ec2133096e9
BobbySU <radio_20bg@yahoo.com>
2022-06-14 20:06:39 +0300

    Add Dynamic Validation

--- DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange({target}) {
+        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (pattern.test(target.value)) {
+            target.classList.remove('error');
+        } else {
+            target.classList.add('error');
+        }
+    }
 }
