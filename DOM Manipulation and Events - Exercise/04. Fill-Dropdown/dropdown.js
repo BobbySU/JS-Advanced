3e66215c4aa3e170f1b3cd2a3a64dd31605af11f
BobbySU <radio_20bg@yahoo.com>
2022-06-16 00:01:06 +0300

    Add Fill Dropdown

--- DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let textInput = document.getElementById('newItemText');
+    let valueInput = document.getElementById('newItemValue');
+
+    let optionElenment = document.createElement('option');
+    optionElenment.textContent = textInput.value;
+    optionElenment.value = valueInput.value;
+    document.getElementById('menu').appendChild(optionElenment)
+
+    textInput.value = '';
+    valueInput.value = '';
 }
