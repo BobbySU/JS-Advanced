55fdd3818674045a82e0ca1e09c43a914145e93c
BobbySU <radio_20bg@yahoo.com>
2022-06-15 23:12:36 +0300

    Add Locked Profile

--- DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button')).forEach(btn => btn.addEventListener('click', onClick));
+
+    function onClick(event) {
+        const profile = event.target.parentElement;
+        const isAcyive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+        if (isAcyive) {
+            let div = profile.querySelector('div');
+            if (event.target.textContent == 'Show more') {
+                div.style.display = 'block';
+                event.target.textContent = 'Hide it';
+            } else {
+                div.style.display = '';
+                event.target.textContent = 'Show more';
+            }
+        }
+    }
 }
