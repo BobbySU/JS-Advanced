566cfc9dab091b2316ec303c12ef33701c279bf5
BobbySU <radio_20bg@yahoo.com>
2022-06-16 00:31:13 +0300

    Add Encode and Decode Messages

--- DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let textArea = document.querySelectorAll('textarea');
+    let buttons = document.querySelectorAll('button');
+
+    buttons[0].addEventListener('click', code);
+    buttons[1].addEventListener('click', decode);
+
+    function code(event) {
+        let textCode = '';
+        let text = textArea[0].value;
+        for (let i = 0; i < text.length; i++) {
+            textCode += String.fromCharCode(text[i].charCodeAt(0) + 1)
+        }
+        textArea[1].value = textCode;
+        textArea[0].value = '';
+    }
+
+    function decode(event) {
+        let text = '';
+        let textCode = textArea[1].value;
+        for (let i = 0; i < textCode.length; i++) {
+            text += String.fromCharCode(textCode[i].charCodeAt(0) - 1)
+        }
+        textArea[1].value = text;
+    }
 }
