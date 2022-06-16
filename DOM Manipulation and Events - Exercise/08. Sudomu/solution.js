ee5b9584efe556e9bb406b1bfcb007f0097c987c
BobbySU <radio_20bg@yahoo.com>
2022-06-16 03:13:12 +0300

    Add Sudomu

--- DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
+++ DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
@@ -4,34 +4,34 @@ function solve() {
     let firstRow = document.querySelectorAll('tr')[2];
     let secondRow = document.querySelectorAll('tr')[3];
     let tirthRow = document.querySelectorAll('tr')[4];
+
     function check() {
         let firsRowValues = firstRow.querySelectorAll('td input');
-        let sum = Number(firsRowValues[0].value) 
-            + Number(firsRowValues[1].value) 
+        let sum = Number(firsRowValues[0].value)
+            + Number(firsRowValues[1].value)
             + Number(firsRowValues[2].value);
-        let firstColumnSum = 
+        let firstColumnSum =
             Number(firstRow.querySelectorAll('td input')[0].value)
             + Number(secondRow.querySelectorAll('td input')[0].value)
             + Number(tirthRow.querySelectorAll('td input')[0].value);
-        let secondColumnSum = 
+        let secondColumnSum =
             Number(firstRow.querySelectorAll('td input')[1].value)
             + Number(secondRow.querySelectorAll('td input')[1].value)
             + Number(tirthRow.querySelectorAll('td input')[1].value);
-        let tirthColumnSum = 
+        let tirthColumnSum =
             Number(firstRow.querySelectorAll('td input')[2].value)
             + Number(secondRow.querySelectorAll('td input')[2].value)
             + Number(tirthRow.querySelectorAll('td input')[2].value);
-            console.log(sum);
-        if (sum == firstColumnSum && 
+        console.log(sum);
+        if (sum == firstColumnSum &&
             sum != 0 &&
             firstColumnSum == secondColumnSum &&
             secondColumnSum == tirthColumnSum) {
-                document.querySelector('#check p').textContent =
+            document.querySelector('#check p').textContent =
                 "You solve it! Congratulations!";
-                document.querySelector('#check p').style.color = 'green';
-                document.querySelector('table').style.border = "2px solid green";
-        }
-        else {
+            document.querySelector('#check p').style.color = 'green';
+            document.querySelector('table').style.border = "2px solid green";
+        } else {
             document.querySelector('#check p').textContent =
                 "NOP! You are not done yet...";
             document.querySelector('#check p').style.color = 'red';
@@ -45,7 +45,7 @@ function solve() {
         tirthRow.querySelectorAll('td input')[0].value = '';
         firstRow.querySelectorAll('td input')[1].value = '';
         secondRow.querySelectorAll('td input')[1].value = '';
-        tirthRow.querySelectorAll('td input')[1].value = '';        
+        tirthRow.querySelectorAll('td input')[1].value = '';
         firstRow.querySelectorAll('td input')[2].value = '';
         secondRow.querySelectorAll('td input')[2].value = '';
         tirthRow.querySelectorAll('td input')[2].value = '';
