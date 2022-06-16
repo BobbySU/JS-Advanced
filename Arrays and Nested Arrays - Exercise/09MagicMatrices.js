9602d52a71b3a1cfa12a9f9aad839f5da88c2f78
BobbySU <radio_20bg@yahoo.com>
2022-06-17 00:13:01 +0300

    Update Magic Matrices

--- Arrays and Nested Arrays - Exercise/09MagicMatrices.js
+++ Arrays and Nested Arrays - Exercise/09MagicMatrices.js
@@ -1,15 +1,25 @@
 function solve(matrix) {
-    let result = false;
     let sum = 0;
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-         sum += matrix[j][i]
-        } 
+    matrix[0].forEach(x => sum += x);
+
+    for (let row = 1; row < matrix.length; row++) {
+        let rowSum = 0;
+        matrix[row].forEach(x => rowSum += x);
+        if (rowSum != sum) {
+            return false;
+        }
     }
-    if(sum%9==0){
-        result = true;
+
+    for (let col = 0; col < matrix[0].length; col++) {
+        let colSum = 0;
+        for (let row = 0; row < matrix.length; row++) {
+            colSum += matrix[row][col];
+        }
+        if (colSum != sum) {
+            return false;
+        }
     }
-    return result;
+    return true;
 }
 
 console.log(solve([[4, 5, 6],
