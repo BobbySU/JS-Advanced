b1221b1a82f0521566499d1f6b2ed18187d05e15
BobbySU <radio_20bg@yahoo.com>
2022-06-17 00:12:16 +0300

    Update Extract Increasing Subsequence from Array

--- Arrays and Nested Arrays - Exercise/05ExtractIncreasingSubsequenceFromArray.js
+++ Arrays and Nested Arrays - Exercise/05ExtractIncreasingSubsequenceFromArray.js
@@ -1,18 +1,18 @@
 function solve(matrix) {
     let result = []
     if (matrix.length >= 0) {
-        result = [matrix[0]]
-        let maxNum = matrix[0]
-        for (let i = 0; i < matrix.length; i++) {
-            if (matrix[i] > maxNum) {
-                result.push(matrix[i])
-                maxNum = matrix[i]
+        let maxNum = matrix[0];
+        for (let i = 1; i < matrix.length; i++) {
+            if (matrix[i] >= maxNum) {
+                result.push(maxNum);
+                maxNum = matrix[i];
             }
         }
+        result.push(maxNum);
     }
     return result;
 }
- 
+
 console.log(solve([1,
     3,
     8,
