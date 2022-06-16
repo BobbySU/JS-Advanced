fa9cd5d846b1ed67073e90553bd6cba36e60e088
BobbySU <radio_20bg@yahoo.com>
2022-06-16 23:11:25 +0300

    Add Add

---
+++ Advanced Functions - Lab/02Add.js
@@ -0,0 +1,15 @@
+function solution(number) {
+    let sum = number;
+    let func = function result(numberToAdd) {
+        return sum + numberToAdd;
+    }
+    return func;
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
