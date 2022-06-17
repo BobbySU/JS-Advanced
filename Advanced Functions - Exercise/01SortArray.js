ecf68cecb28a732abb56652e609570bbad394d65
BobbySU <radio_20bg@yahoo.com>
2022-06-17 21:14:29 +0300

    Update Sort Array

--- Advanced Functions - Exercise/01SortArray.js
+++ Advanced Functions - Exercise/01SortArray.js
@@ -1,12 +1,22 @@
-function solve(input, type){
-    if(type=='asc'){
-        input.sort((a,b)=> a-b);
-    }else{
-        input.sort((a,b)=> b-a);
+function solve(input, type) {
+    if (type == 'asc') {
+        input.sort((a, b) => a - b);
+    } else {
+        input.sort((a, b) => b - a);
     }
     return input;
 }
 
+function solve2(input, type) {
+    let method = {
+        asc: (a, b) => a - b,
+        desc: (a, b) => b - a
+    }
+    input.sort(method[type])
+    return input;
+}
+
+
 console.log(solve([14, 7, 17, 6, 8], 'asc'));
 
-console.log(solve([14, 7, 17, 6, 8], 'desc'));
+console.log(solve2([14, 7, 17, 6, 8], 'desc'));
