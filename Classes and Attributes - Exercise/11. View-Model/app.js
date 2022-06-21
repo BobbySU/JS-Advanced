dfbfccf02162aa2acb74fc336dadd66a71dd9aa8
BobbySU <radio_20bg@yahoo.com>
2022-06-22 00:18:00 +0300

    Add View Model

--- Classes and Attributes - Exercise/11. View-Model/app.js
+++ Classes and Attributes - Exercise/11. View-Model/app.js
@@ -1,8 +1,32 @@
 class Textbox {
-    //TODO
+    constructor(selector, regex) {
+        this._value;
+        this._elements = Array.from(document.querySelectorAll(selector));
+        this._invalidSymbols = regex;
+    }
+
+    set value(value) {
+        this._value = value;
+        let els = this._elements;
+        els.forEach(el => el.value = value);
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    get elements() {
+        return this._elements;
+    }
+
+    isValid() {
+        return !this._invalidSymbols.test(this.value);
+    }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
 let inputs = document.getElementsByClassName('.textbox');
 
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+inputs.addEventListener('click', function () {
+    console.log(textbox.value);
+});
